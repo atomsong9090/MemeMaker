8a1c4891ad75e1d9f1cee1881d2d6a9aa7036e88
atomsong92 <yesparrot@gmail.com>
2021-03-23 22:36:20 +0900

    [client] ADD Content Components 개발
    Meme를 볼 수 있는 Content box 컴포넌트 개발

--- client/src/pages/main.tsx
+++ client/src/pages/main.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement } from "react";
 import styled from "styled-components";
 import NavBar from "../components/navBar";
+import Content from "../components/content";
 
 export default function Main(): ReactElement {
   return (
     <Page>
       <NavBar />
+      <Content />
     </Page>
   );
 }
